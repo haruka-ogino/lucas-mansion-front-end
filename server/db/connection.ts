c5c08821cec3d161436e8004cf5301e6c0265014
Haruka Ogino <harukariq@gmail.com>
2024-07-19 13:14:16 +1200

    debug env access

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,14 +1,16 @@
 import { createClient } from '@libsql/client'
+import dotenv from 'dotenv'
+dotenv.config()
 
-// Initialize the Turso client
+// Initialise the Turso client
 const tursoDatabaseUrl = process.env.TURSO_DATABASE_URL as string
 const tursoAuthToken = process.env.TURSO_AUTH_TOKEN as string
 
-console.log('Turso Database URL:', tursoDatabaseUrl)
-console.log('Turso Auth Token:', tursoAuthToken)
-
-// Initialize the Turso client
+// Initialise the Turso client
 export const turso = createClient({
   url: tursoDatabaseUrl,
   authToken: tursoAuthToken,
+  // url: 'libsql://lucas-mansion-harukaogn.turso.io',
+  // authToken:
+  //   'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MjEzNTAyNzAsImlkIjoiMTJmZWE0YTEtYzM1MS00MDIwLWE1OGItNjM1NjQ2MGJmMDc4In0.LZ-v5jCYsaHJNXp60BGN8WqozGBDKIrxMtj4e6Ejl9HmecPvEnysLgAikRQgPIeOwkDHPJ0gVGYOZNCFtjiiDw',
 })
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,3 @@
-import path from 'path'
-import url from 'node:url'
-import dotenv from 'dotenv'
-
-const __filename = url.fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-dotenv.config({ path: path.join(__dirname, '../.env') })
-
 import server from './server.ts'
 
 const PORT = process.env.PORT || 3000
