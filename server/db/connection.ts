f8f0d68cfc7993f793865107a93075e790c1d2ce
Haruka Ogino <harukariq@gmail.com>
2024-07-19 19:15:02 +1200

    update connection file

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -10,7 +10,4 @@ const tursoAuthToken = process.env.TURSO_AUTH_TOKEN as string
 export const turso = createClient({
   url: tursoDatabaseUrl,
   authToken: tursoAuthToken,
-  // url: 'libsql://lucas-mansion-harukaogn.turso.io',
-  // authToken:
-  //   'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MjEzNTAyNzAsImlkIjoiMTJmZWE0YTEtYzM1MS00MDIwLWE1OGItNjM1NjQ2MGJmMDc4In0.LZ-v5jCYsaHJNXp60BGN8WqozGBDKIrxMtj4e6Ejl9HmecPvEnysLgAikRQgPIeOwkDHPJ0gVGYOZNCFtjiiDw',
 })
